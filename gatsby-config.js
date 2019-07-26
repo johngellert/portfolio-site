f51e89063a17483d33579e94b72f4250c764a9ef
John Gellert <gellertjm@gmail.com>
2019-07-26 12:26:22 -0500

    Remove  background and theme color.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        // background_color: '#663399',
+        // theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/profile-icon.png', // This path is relative to the root of the site.
       },
